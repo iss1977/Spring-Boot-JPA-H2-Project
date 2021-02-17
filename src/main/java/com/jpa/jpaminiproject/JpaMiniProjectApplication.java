e1e37a6024d9fd7ce09b258e641e5bc27d7d783d
iss1977 <ioan.szucs@gmail.com>
2021-02-17 17:24:04 +0100

    Starting the project

---
+++ src/main/java/com/jpa/jpaminiproject/JpaMiniProjectApplication.java
@@ -0,0 +1,13 @@
+package com.jpa.jpaminiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaMiniProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaMiniProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jpa/jpaminiproject/JpaMiniProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpa.jpaminiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaMiniProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
